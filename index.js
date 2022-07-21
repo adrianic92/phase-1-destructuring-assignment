086eacf0a4dbba00e1728fc4d2431ee233b6c185
Adrian <adrianic92@yahoo.com>
2022-07-21 15:58:26 -0700

    commit tree 8392c1d69e0d90e33c6b5d67c473a3d79f10432c

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const farmAnimals = 'cow horse sheep pig chicken';
+const farmAnimalsArray = farmAnimals.split(" ");
+const [moo, neigh, baa, oink, cluck] = farmAnimalsArray;
+const [bessie, , dolly, babe, little] = farmAnimalsArray;
+const [blackAndWhite, , black, pink] = farmAnimalsArray;
 
 const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+const [red, orange, yellow, green, blue, indigo, violet] = colors;
+const [r, o, y, g, b, , v] = colors;
+const [, , , , , indg, ] = colors;
 
 const muppet = {
   muppetName: 'Miss Piggy',
@@ -9,6 +16,9 @@ const muppet = {
   job: 'Cast member of The Muppet Show',
   partner: 'Kermit'
 };
+
+const {muppetName, color, song, job, partner} = muppet;
+
 
 const nestedMuppet = {
   nestedName: 'Kermit',
@@ -24,6 +34,9 @@ const nestedMuppet = {
   nestedJob: 'Host of The Muppet Show',
   nestedPartner: 'Miss Piggy'
 };
+
+const {song2, song4} = nestedMuppet.album.theMuppetMovie;
+const {nestedJob, nestedPartner} = nestedMuppet;
 
 // Strings
 
